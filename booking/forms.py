fb97cebffdd214a84249cce5ce4dd13af9b4656f
Hasibullah Fathi <hasibullah.fathi@gmail.com>
2024-08-25 15:01:33 +0000

    alter tabels

--- booking/forms.py
+++ booking/forms.py
@@ -5,7 +5,7 @@ from .models import Booking
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
-        fields = ['table', 'booking_date', 'booking_time', 'number_of_guests', 'remarks']
+        fields = ['table', 'booking_date', 'phone_number', 'booking_time', 'number_of_guests', 'remarks']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
--- booking/models.py
+++ booking/models.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_number = models.CharField(max_length=15)
     ROLE_CHOICES = ((1, 'Admin'), (0, 'Customer'))
     role = models.IntegerField(choices=ROLE_CHOICES, default=0)
 
@@ -21,11 +20,11 @@ class Table(models.Model):
     def __str__(self):
         return f"Table {self.table_number}"
 
-
 # 3. Booking Model
 class Booking(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)  # The user who makes the booking
-    table = models.ForeignKey(Table, on_delete=models.CASCADE)  # The table being booked
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15, null=True) 
     booking_date = models.DateField()
     booking_time = models.TimeField()
     created_on = models.DateTimeField(auto_now_add=True)
