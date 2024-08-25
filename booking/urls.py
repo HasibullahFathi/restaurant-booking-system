f5f2c5acfaba8d13cf7a6c08598eeb5f7e16dab2
Hasibullah Fathi <hasibullah.fathi@gmail.com>
2024-08-25 08:33:53 +0000

    create booking details page

--- booking/urls.py
+++ booking/urls.py
@@ -1,10 +1,10 @@
 from . import views
 from django.urls import path
-from booking.views import BookingList
+from booking.views import BookingList, BookingDetail
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('mybooking/', views.book_table, name='book_table'),
     path('bookings/', BookingList.as_view(), name='booking_list'),
-
+    path('bookings/<int:pk>/', BookingDetail.as_view(), name='booking_detail'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
 from django.http import HttpResponse
 from.models import Booking
@@ -11,6 +11,10 @@ class BookingList(ListView):
     context_object_name = 'bookings'  # This sets the name of the context variable
     paginate_by = 8
 
+class BookingDetail(DetailView):
+    model = Booking
+    template_name = 'booking/booking_detail.html'  # The template to render
+    context_object_name = 'booking'  # The context variable for the template
 
 def index(request):
     return render(request, 'booking/index.html')
