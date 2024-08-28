bd3839f2ca0039678d0dc099723bcf7fde8e1b30
Hasibullah Fathi <hasibullah.fathi@gmail.com>
2024-08-28 11:32:55 +0000

    add edit and delete categories

--- menu/urls.py
+++ menu/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.menu_category_list, name='menu_category_list'),
     path('create/category/', views.create_menu_category, name='create_menu_category'),
     path('create/item/', views.create_menu_item, name='create_menu_item'),
+    path('edit/<int:category_id>/', views.edit_menu_category, name='edit_menu_category'),
+    path('delete/<int:category_id>/', views.delete_menu_category, name='delete_menu_category'),
     
 ]
--- menu/views.py
+++ menu/views.py
@@ -4,10 +4,11 @@ from .models import MenuCategory, MenuItem
 from .forms import MenuCategoryForm, MenuItemForm
 from django.contrib import messages
 
+
 def menu_category_list(request):
     categories = MenuCategory.objects.prefetch_related('menu_items').all()
     return render(request, 'menu/menu.html', {'categories': categories})
-    
+
 
 @login_required
 def create_menu_category(request):
@@ -45,3 +46,42 @@ def create_menu_item(request):
     }
     return render(request, 'menu/create_menu_item.html', context)
 
+
+@login_required
+def edit_menu_category(request, category_id):
+    if not request.user.is_staff:
+        return redirect('menu_category_list')  # Redirect to a page that handles unauthorized access
+
+    # Fetch the category or return a 404 if not found
+    category = get_object_or_404(MenuCategory, id=category_id)
+
+    if request.method == 'POST':
+        category_form = MenuCategoryForm(request.POST, instance=category)
+        if category_form.is_valid():
+            category_form.save()
+            return redirect('menu_category_list')  # Redirect to a list or detail page after saving
+    else:
+        category_form = MenuCategoryForm(instance=category)
+
+    context = {
+        'category_form': category_form,
+        'category': category,
+    }
+    return render(request, 'menu/edit_menu_category.html', context)
+
+
+@login_required
+def delete_menu_category(request, category_id):
+    if not request.user.is_staff:
+        return redirect('menu_category_list')  # Redirect if the user is not an admin
+
+    category = get_object_or_404(MenuCategory, id=category_id)
+
+    if request.method == 'POST':
+        category.delete()
+        return redirect('menu_category_list')  # Redirect after successful deletion
+
+    context = {
+        'category': category,
+    }
+    return render('menu_category_list')
