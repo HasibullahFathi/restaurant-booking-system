c9edaa605b4fc0f01aa8a14d23da87579d120539
Hasibullah Fathi <hasibullah.fathi@gmail.com>
2024-09-01 22:27:50 +0000

    validate python code of booking app

--- booking/urls.py
+++ booking/urls.py
@@ -1,12 +1,23 @@
-from . import views
 from django.urls import path
-from booking.views import BookingList, BookingDetail, create_booking
+from . import views
+from booking.views import (
+    BookingList, BookingDetail, create_booking, 
+    delete_booking, edit_booking, index
+)
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('bookings/create/', views.create_booking, name='booking_form'),
-    path('edit_booking/<int:booking_id>/', views.edit_booking, name='edit_booking'),
     path('bookings/', BookingList.as_view(), name='booking_list'),
+    path('bookings/create/', create_booking, name='booking_form'),
     path('bookings/<int:pk>/', BookingDetail.as_view(), name='booking_detail'),
-    path('delete_booking/<int:booking_id>/', views.delete_booking, name='delete_booking'),
+    path(
+        'delete_booking/<int:booking_id>/',
+        delete_booking,
+        name='delete_booking'
+    ),
+    path(
+        'edit_booking/<int:booking_id>/',
+        edit_booking,
+        name='edit_booking'
+    ),
+    path('', index, name='home'),
 ]
