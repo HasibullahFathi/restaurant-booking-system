5a309322cba82efb8ada47e6299dbf0efc4a6786
Hasibullah Fathi <hasibullah.fathi@gmail.com>
2024-09-03 17:47:12 +0000

    remove 404 page

--- red_chillies/settings.py
+++ red_chillies/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-hasibullahf-restaurantb-t41lrpcwpkx.ws.codeinstitute-ide.net', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-hasibullahf-restaurantb-t41lrpcwpkx.ws.codeinstitute-ide.net', '.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
